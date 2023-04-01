c2d42053743a4eb3b1227097a1ed311e732f20fd
perkles <loic.lemos@gmail.com>
2023-04-01 19:59:46 -0300

    atualizando

---
+++ WidgetsPersonalizados/widgets_personalizados/lib/one_page.dart
@@ -0,0 +1,24 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:widgets_personalizados/widgets/custom_button_widgets.dart';
+
+class OnePage extends StatelessWidget {
+  const OnePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: CustomButtonWidget(
+          disable: false,
+          onPressed: () {},
+          title: 'Custom BTN',
+          titleSize: 18,
+          ),
+      ),
+    );
+  }
+}
